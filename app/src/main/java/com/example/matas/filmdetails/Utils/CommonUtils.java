8fe84ec485ed6a492c5b611627c10a20708f39fe
Matas <matas.vireikis@hotmail.com>
2018-08-28 23:47:49 +0100

    Initial commit

---
+++ app/src/main/java/com/example/matas/filmdetails/Utils/CommonUtils.java
@@ -0,0 +1,28 @@
+package com.example.matas.filmdetails.Utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.view.inputmethod.InputMethodManager;
+
+
+public class CommonUtils {
+
+    //Hide the keyboard
+    public static void hideSoftKeyboard(Activity activity) {
+        InputMethodManager inputMethodManager = (InputMethodManager)  activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), 0);
+    }
+
+    //Return true if network is available or about to become available
+
+    static public boolean isNetworkAvailable(Context c) {
+        ConnectivityManager cm =
+                (ConnectivityManager)c.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+    }
+}
